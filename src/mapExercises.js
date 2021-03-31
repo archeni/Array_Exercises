b9fb5b7acc26a9f611b82d4c70b9326e4d2cc0c0
Traye <johnson.traye@yahoo.com>
2021-03-31 16:00:15 -0500

    added map and find functions

--- src/mapExercises.js
+++ src/mapExercises.js
@@ -3,10 +3,27 @@ import { useStudents, useInstructors } from "./data/classroom.js";
 const students = useStudents();
 const instructors = useInstructors();
 
+export const getStudentNames = () => {
+  const studentNames = students.map(student => 
+    `${student.firstName} ${student.lastName}`)
+  return studentNames
+}
 // Export a function named getStudentNames
 // It should not accept any parameters`
 // It should return an array of strings. Each string should be the full name of the student
 
+
+export const StudentList = () => {
+  const studentHtml = students.map(student => 
+    `
+    <div>
+      <h1>${student.firstName} ${student.lastName}</h1>
+      <h2>Cohort ${student.cohort}</h2>
+    </div>`
+  ).join('')
+
+  return studentHtml
+}
 // Export a function named StudentList
 // It should not accept any parameters
 // It should return a single HTMLString that contains a <div> element for every student that looks like the following:
